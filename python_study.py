f63f35167eaf8e2e3f9a03f72c1d6cd4e71fd00c
Alexei Yashkov <alexei.yashkov@yashkov.org>
2025-05-10 13:07:54 -0400

    Serve static files

--- python_study.py
+++ python_study.py
@@ -1,9 +1,14 @@
+from pathlib import Path
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
 app = FastAPI()
+api = FastAPI()
+app.mount("/api/v1", api)
+app.mount("/", StaticFiles(directory=Path("www"), html=True))
 
 
 class Item(BaseModel):
@@ -12,16 +17,11 @@ class Item(BaseModel):
     is_offer: Union[bool, None] = None
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
+@api.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
-@app.put("/items/{item_id}")
+@api.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
