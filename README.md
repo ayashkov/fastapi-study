a990533c2fd12d78a69d6ac8dbe111bcf60fb587
Alexei Yashkov <alexei.yashkov@yashkov.org>
2025-05-11 11:21:27 -0400

    Add run script, exclude Angular sources from wheel

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Python Study
+# Python SPA Study
 
-A study of Python, uv, and FastAPI.
+A study of Python, uv, FastAPI, and Angular.
 
 ```shell
 fastapi dev fastapi_study/app.py
--- fastapi_study/app.py
+++ fastapi_study/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from uvicorn import run
 
 from fastapi_study.api import api
 from fastapi_study.spa import SinglePageApplication
@@ -6,4 +7,8 @@ from fastapi_study.spa import SinglePageApplication
 app = FastAPI()
 app.include_router(api, prefix="/api/v1")
 app.mount("/", SinglePageApplication(packages=["fastapi_study.ui"]),
-    name="Angular")
+          name="Angular")
+
+
+def main() -> None:
+    run(app, host="0.0.0.0")
